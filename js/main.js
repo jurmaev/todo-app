fefe27ba13259489a0ed509f049f9ba3c141b0f6
jurmaev <smartemii03@gmail.com>
2023-11-03 21:06:22 +0500

    save todos to local storage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 (() => {
-  let todos;
+  let todos = JSON.parse(localStorage.getItem('todos'))
+    ? JSON.parse(localStorage.getItem('todos'))
+    : [];
+
   const createTodo = document.querySelector('.input');
   const todosList = document.querySelector('.todos');
+
+  addTodos(todos);
+
   createTodo.addEventListener('keydown', (e) => {
     if (e.key !== 'Enter') return;
-    const item = document.createElement('li');
-    item.classList.add('todos__item');
-    item.textContent = createTodo.value;
+
+    const todoValue = createTodo.value.trim();
+    if (!todoValue) return;
+
+    todos.push({ value: todoValue, isCompleted: false });
+    localStorage.setItem('todos', JSON.stringify(todos));
     createTodo.value = '';
-    todosList.append(item);
+    addTodos(todos);
   });
+
+  function addTodos(todos) {
+    todos.forEach((todo) => {
+      const item = document.createElement('li');
+      item.classList.add('todos__item');
+      item.textContent = todo.value;
+      todosList.append(item);
+    });
+  }
 })();
