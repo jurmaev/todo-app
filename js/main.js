d9402564bf969d0e1aa06c03a96cdef0f8d6bd7d
jurmaev <smartemii03@gmail.com>
2023-11-03 21:57:12 +0500

    add todo deletion

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     : [];
 
   const createTodo = document.querySelector('.input');
-  const todosList = document.querySelector('.todos');
+  const todosList = document.querySelector('.todo__list');
 
   addTodos(todos);
 
@@ -13,19 +13,45 @@
 
     const todoValue = createTodo.value.trim();
     if (!todoValue) return;
-
-    todos.push({ value: todoValue, isCompleted: false });
+    currentTodo = {
+      id: crypto.randomUUID(),
+      value: todoValue,
+      isCompleted: false,
+    };
+    todos.push(currentTodo);
     localStorage.setItem('todos', JSON.stringify(todos));
     createTodo.value = '';
-    addTodos(todos);
+    addTodos([currentTodo]);
   });
 
   function addTodos(todos) {
     todos.forEach((todo) => {
+      const todoContainer = document.createElement('div');
       const item = document.createElement('li');
-      item.classList.add('todos__item');
+      const closeBtn = document.createElement('button');
+      const closeImg = document.createElement('img');
+
+      closeImg.src = 'assets/close.svg';
+      closeImg.alt = 'Delete todo';
+      closeImg.classList.add('todo__close');
+
+      closeBtn.classList.add('todo__btn');
+      todoContainer.classList.add('todo__container');
+      todoContainer.id = todo.id;
+
+      item.classList.add('todo__item');
       item.textContent = todo.value;
-      todosList.append(item);
+      closeBtn.append(closeImg);
+      todoContainer.append(item);
+      todoContainer.append(closeBtn);
+      todosList.append(todoContainer);
+
+      closeBtn.addEventListener('click', (e) => {
+        const id = e.target.parentElement.parentElement.id;
+        e.target.parentElement.parentElement.remove();
+        todos = todos.filter((todo) => todo.id !== id);
+        localStorage.setItem('todos', JSON.stringify(todos));
+      });
     });
   }
 })();
